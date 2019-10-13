d4e4cb50905163987f96f78fc793ce7cef8f9026
2trvl <38960745+2trvl@users.noreply.github.com>
2019-10-13 16:36:42 +0300

    Add test folder

---
+++ test/code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    printf("%d", a&b&c);
+    return 0;
+}
